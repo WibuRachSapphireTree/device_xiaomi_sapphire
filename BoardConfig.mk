9b47d8abc66ebe621120f892f780feb7f2ec7fa3
HiSiuDz <112377723+Huy7684@users.noreply.github.com>
2026-09-11 12:55:28 +0700

    Remove empty line in BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,7 +10,6 @@ include vendor/xiaomi/sapphire/BoardConfigVendor.mk
 DEVICE_PATH := device/xiaomi/sapphire
 KERNEL_PATH := $(DEVICE_PATH)-kernel
 
-
 BUILD_BROKEN_DUP_RULES := true
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 BUILD_BROKEN_INCORRECT_PARTITION_IMAGES := true
